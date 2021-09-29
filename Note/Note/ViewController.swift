b9bb3809a363a8fedb8f5cafaad4f777917468cc
haiphan5289 <haiphan5289@gmail.com>
2021-09-29 20:20:34 +0700

    init

---
+++ Note/Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by haiphan on 29/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
