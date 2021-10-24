29653fc7fe9d0c21a362d87c5b263e021d39b9b8
haiphan5289 <haiphan5289@gmail.com>
2021-10-24 17:42:23 +0700

    update UI Calendar Picker

---
+++ Note/Note/Views/CalendarView/CalendarPickerModel.swift
@@ -0,0 +1,25 @@
+//
+//  CalendarPickerModel.swift
+//  Note
+//
+//  Created by haiphan on 24/10/2021.
+//
+
+import Foundation
+
+struct Day {
+  // 1
+  let date: Date
+  // 2
+  let number: String
+  // 3
+  let isSelected: Bool
+  // 4
+  let isWithinDisplayedMonth: Bool
+}
+
+struct MonthMetadata {
+  let numberOfDays: Int
+  let firstDay: Date
+  let firstDayWeekday: Int
+}
--- Note/Note/Views/CalendarView/Cell/CalendarCell.swift
+++ Note/Note/Views/CalendarView/Cell/CalendarCell.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class CalendarCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var lbDay: UILabel!
+    
+    private lazy var accessibilityDateFormatter: DateFormatter = {
+      let dateFormatter = DateFormatter()
+      dateFormatter.calendar = Calendar(identifier: .gregorian)
+      dateFormatter.setLocalizedDateFormatFromTemplate("EEEE, MMMM d")
+      return dateFormatter
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func updateValue(day: Day) {
+        self.lbDay.text = day.number
+        accessibilityLabel = accessibilityLabel
+        accessibilityLabel = accessibilityDateFormatter.string(from: day.date)
+        self.applyDefaultStyle(isWithinDisplayedMonth: day.isWithinDisplayedMonth)
+    }
+    
+    private func applyDefaultStyle(isWithinDisplayedMonth: Bool) {
+      accessibilityTraits.remove(.selected)
+      accessibilityHint = "Tap to select"
+
+        self.lbDay.textColor = isWithinDisplayedMonth ? .label : .secondaryLabel
+//      self.isHidden = true
     }
 
 }
