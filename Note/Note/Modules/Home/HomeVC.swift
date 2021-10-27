5ff7263ca67dd6a73e8add1d82dbb95c0911dcce
haiphan5289 <haiphan5289@gmail.com>
2021-10-27 10:25:01 +0700

    update important --- multi cell

--- Note/Note/Modules/Home/HomeVC.swift
+++ Note/Note/Modules/Home/HomeVC.swift
@@ -33,7 +33,7 @@ class HomeVC: BaseNavigationHome {
     private let vAddNote: AddNote = AddNote.loadXib()
     private let vDropDown: DropdownView = DropdownView(frame: .zero)
     
-    private var listNote: [NoteModel] = []
+    @VariableReplay private var listNote: [NoteModel] = []
     private let eventStatusDropDown: PublishSubject<AddNote.StatusAddNote> = PublishSubject.init()
     private var selectIndexs: [IndexPath] = []
     private var audio: AVAudioPlayer = AVAudioPlayer()
@@ -80,7 +80,6 @@ extension HomeVC {
         }
         
         self.collectionView.delegate = self
-        self.collectionView.dataSource = self
         self.collectionView.register(HomeTextCell.nib, forCellWithReuseIdentifier: HomeTextCell.identifier)
         self.collectionView.register(CheckListCell.nib, forCellWithReuseIdentifier: CheckListCell.identifier)
         self.collectionView.register(DrawHomeCell.nib, forCellWithReuseIdentifier: DrawHomeCell.identifier)
@@ -129,6 +128,91 @@ extension HomeVC {
             
             wSelf.listNote = list
             wSelf.collectionView.reloadData()
+        }.disposed(by: disposeBag)
+        
+        self.$listNote.bind(to: collectionView.rx.items) {
+          (collectionView: UICollectionView, index: Int, note: NoteModel) in
+            let indexPath = IndexPath(item: index, section: 0)
+            switch note.noteType {
+            case .checkList:
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CheckListCell.identifier, for: indexPath) as? CheckListCell else {
+                    fatalError("Don't have Cell")
+                }
+                cell.layoutIfNeeded()
+                cell.updateValue(note: note)
+                cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
+                let hasSelect = self.selectIndexs.contains(indexPath)
+                let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
+                cell.imgSelect.image = img
+                cell.layoutIfNeeded()
+                return cell
+            case .text:
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeTextCell.identifier, for: indexPath) as? HomeTextCell else {
+                    fatalError("Don't have Cell")
+                }
+                cell.layoutIfNeeded()
+                cell.updateValue(note: note)
+                cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
+                
+                let hasSelect = self.selectIndexs.contains(indexPath)
+                let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
+                cell.imgSelect.image = img
+                cell.layoutIfNeeded()
+                return cell
+                
+            case .draw:
+                
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DrawHomeCell.identifier, for: indexPath) as? DrawHomeCell else {
+                    fatalError("Don't have Cell")
+                }
+                cell.layoutIfNeeded()
+                if let model = note.noteDrawModel {
+                    cell.updateValueNote(noteModel: model)
+                } else {
+                    cell.img.isHidden = true
+                }
+                
+                cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
+                let hasSelect = self.selectIndexs.contains(indexPath)
+                let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
+                cell.imgSelect.image = img
+                
+                cell.layoutIfNeeded()
+                return cell
+                
+            case .photo:
+                
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as? PhotoCell else {
+                    fatalError("Don't have Cell")
+                }
+                
+                
+                cell.layoutIfNeeded()
+                cell.updateValue(note: note)
+                cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
+                
+                let hasSelect = self.selectIndexs.contains(indexPath)
+                let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
+                cell.imgSelect.image = img
+                cell.layoutIfNeeded()
+                return cell
+                
+            default:
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeTextCell.identifier, for: indexPath) as? HomeTextCell else {
+                    fatalError("Don't have Cell")
+                }
+                return cell
+            }
+            }.disposed(by: disposeBag)
+        
+        self.collectionView.rx.itemSelected.bind { [weak self] idx in
+            guard let wSelf = self else { return }
+            switch wSelf.navigationItemView.actionStatus {
+            case .normal:
+                wSelf.moveToNote(idx: idx)
+            case .edit:
+                wSelf.addOrRemoveNote(idx: idx)
+            }
         }.disposed(by: disposeBag)
         
         self.eventStatusDropDown.asObservable().bind { [weak self] status in
@@ -355,100 +439,6 @@ extension HomeVC: DropDownDelegate {
             self.navigationController?.pushViewController(vc, animated: true)
         case .photo:
             self.presentImagePicker()
-        default: break
-        }
-    }
-    
-    
-}
-extension HomeVC: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.listNote.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let note = self.listNote[indexPath.row]
-    
-        switch note.noteType {
-        case .checkList:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CheckListCell.identifier, for: indexPath) as? CheckListCell else {
-                fatalError("Don't have Cell")
-            }
-            cell.layoutIfNeeded()
-            cell.updateValue(note: note)
-            cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
-            let hasSelect = self.selectIndexs.contains(indexPath)
-            let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
-            cell.imgSelect.image = img
-            cell.layoutIfNeeded()
-            return cell
-        case .text:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeTextCell.identifier, for: indexPath) as? HomeTextCell else {
-                fatalError("Don't have Cell")
-            }
-            cell.layoutIfNeeded()
-            cell.updateValue(note: note)
-            cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
-            
-            let hasSelect = self.selectIndexs.contains(indexPath)
-            let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
-            cell.imgSelect.image = img
-            cell.layoutIfNeeded()
-            return cell
-            
-        case .draw:
-            
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DrawHomeCell.identifier, for: indexPath) as? DrawHomeCell else {
-                fatalError("Don't have Cell")
-            }
-            cell.layoutIfNeeded()
-            if let model = note.noteDrawModel {
-                cell.updateValueNote(noteModel: model)
-            } else {
-                cell.img.isHidden = true
-            }
-            
-            cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
-            let hasSelect = self.selectIndexs.contains(indexPath)
-            let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
-            cell.imgSelect.image = img
-            
-            cell.layoutIfNeeded()
-            return cell
-            
-        case .photo:
-            
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as? PhotoCell else {
-                fatalError("Don't have Cell")
-            }
-            
-            
-            cell.layoutIfNeeded()
-            cell.updateValue(note: note)
-            cell.imgSelect.isHidden = (self.navigationItemView.actionStatus == .normal) ? true : false
-            
-            let hasSelect = self.selectIndexs.contains(indexPath)
-            let img = (hasSelect) ? Asset.icCheckbox.image : Asset.icUncheck.image
-            cell.imgSelect.image = img
-            cell.layoutIfNeeded()
-            return cell
-            
-        default:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeTextCell.identifier, for: indexPath) as? HomeTextCell else {
-                fatalError("Don't have Cell")
-            }
-            return cell
-        }
-        
-
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        switch self.navigationItemView.actionStatus {
-        case .normal:
-            self.moveToNote(idx: indexPath)
-        case .edit:
-            self.addOrRemoveNote(idx: indexPath)
         }
     }
     
